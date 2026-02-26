c752bc9df17653ac2950c1842d32df304b7730f5
Vinita2323 <vinijinodiya@gmail.com>
2026-02-26 17:35:44 +0530

    first commit

---
+++ frontend/tailoring-project/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
